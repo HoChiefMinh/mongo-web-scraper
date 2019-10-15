c51ac73c39f3fe7b792b63918477a104498c4ca4
Minh Pham <mqpham848@gmail.com>
2019-10-14 23:58:53 -0400

    index.js

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-    
-})
+$(document).ready(function() {
+  // Setting a reference to the article-containter div where all the dynamic content will go
+  // Adding event listeners to any dynamically generated 'save article'
+  // and 'scrape new article' buttons
+  let articlecontainer = $('.article-container');
+  $(document).on('click', '.btn.save', handleArticleSave);
+  $(document).on('click', 'scrape-new', handleArticleScrape);
+});
