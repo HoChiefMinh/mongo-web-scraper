bd252dd7f1f64d454c66751d46b19793316f7d1d
Minh Pham <mqpham848@gmail.com>
2019-10-14 22:17:43 -0400

    added code to delete and update articles

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -11,66 +11,47 @@ console.log(
     '\n***********************************\n'
 );
 
-// Making a request via axios for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
-axios.get('https://www.wsj.com/news/technology').then(function(response) {
-  // Load the HTML into cheerio and save it to a letiable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  let $ = cheerio.load(response.data);
+let scrape = function(cb) {
 
-  // An empty array to save the data that we'll scrape
-  let results = [];
+  // Making a request via axios for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
+  axios.get('https://www.wsj.com/news/technology').then(function(response) {
+    // Load the HTML into cheerio and save it to a letiable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    let $ = cheerio.load(response.data);
 
-  // With cheerio, find each h3-tag
-  // (i: iterator. element: the current element)
-  $('h3').each(function(i, element) {
-    // Save the text of the element in a "title" letiable
-    let title = $(this)
-      .children()
-      .text();
+    // An empty array to save the data that we'll scrape
+    let results = [];
 
-    // Save the text of the element in a 'summary letiable
-    let summary = $(this) // could not get summary to display to page
-      .find('span')
-      .text();
-
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    let link = $(this)
-      .children('a')
-      .attr('href');
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      summary: summary,
-      link: link
-    });
-
-    // With cheerio, find each p-tag
+    // With cheerio, find each h3-tag
     // (i: iterator. element: the current element)
-    // $('p').each(function(i, element){
+    $('h3').each(function(i, element) {
+      // Save the text of the element in a "title" letiable
+      let title = $(this)
+        .children()
+        .text();
 
-    //   // Save the text of the element in a "summary" letiable
-    //   let summary  = $(element).find('span').text();
+      // Save the text of the element in a 'summary letiable
+      let summary = $(this) // could not get summary to display to page
+        .find('span')
+        .text();
 
-    //  // Save these results in an object that we'll push into the results array we defined earlier
-    //   results.push({
-    //     summary: summary,
-    //   });
-    // });
+      // In the currently selected element, look at its child elements (i.e., its a-tags),
+      // then save the values for any "href" attributes that the child elements may have
+      let link = $(this)
+        .children('a')
+        .attr('href');
 
-    // $('a.wjs-image-link').each(function(i, element){
-
-    //   let imgLink  = $(element).find("img").attr("src");
-
-    //   results.push({
-    //     image: imgLink
-    //   });
-    // });
+      // Save these results in an object that we'll push into the results array we defined earlier
+      results.push({
+        title: title,
+        summary: summary,
+        link: link
+      });
+    });
+    cb(results);
+    // Log the results once you've looped through each of the elements found with cheerio
+    // console.log(results);
   });
+};
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-});
-
-// module.exports = scrape;
+module.exports = scrape;
